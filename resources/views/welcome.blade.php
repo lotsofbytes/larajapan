2b0ed043477c719744dd17e637e66abe79aa1193
Kenji Hino <kenji@lotsofbytes.com>
2017-01-16 21:17:14 -0800

    translated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,8 +68,8 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
+                    <a href="{{ url('/login') }}">ログイン</a>
+                    <a href="{{ url('/register') }}">会員登録</a>
                 </div>
             @endif
 
