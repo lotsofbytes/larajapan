18061864cdea22354d7466dcf92306b63a1ad5c6
Kenji Hino <kenji@lotsofbytes.com>
2017-01-21 15:02:04 -0800

    updated

---
+++ app/Auth/Notifications/ResetPassword.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Auth\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('パスワードリセット')
+            ->greeting('パスワードリセット')
+            ->line('パスワードリセットリンクの送信のリクエストがありました。')
+            ->action('リセットパスワード', url('password/reset', $this->token))
+            ->line('リクエストされていなかったら、無視してください。');
+    }
+}
