b0b84d12313d907c7a6ac6ea9c1fd37627b51f83
Kenji Hino <kenji@lotsofbytes.com>
2017-01-22 20:12:11 -0800

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,7 @@
     <!-- Scripts -->
     <script src="{{ url('/js/app.js') }}"></script>
 
-    @if {{ config('local.analytics') }}
+    @if (config('local.analytics'))
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
