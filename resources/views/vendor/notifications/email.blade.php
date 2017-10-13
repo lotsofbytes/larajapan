8b09ece18bdaf84e300ee44e1599743fb18d89c6
Kenji Hino <kenji@lotsofbytes.com>
2017-10-13 16:59:25 -0700

    translated

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -45,14 +45,13 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+よろしくお願いします。<p>{{ config('app.name') }}</p>
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+"{{ $actionText }}"ボタンが効かない場合は、ブラウザに以下のアドレスをコピー＆ペーストしてください：[{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
