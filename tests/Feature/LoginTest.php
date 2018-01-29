d983ae00d36912df7ab465c5889d17dc9a87bfa8
Kenji Hino <kenji@lotsofbytes.com>
2018-01-29 14:52:44 -0800

    login tests

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -7,17 +7,19 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class LoginTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    /** @test */
+    public function user_can_view_login()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function unauthenticated_user_cannot_view_home()
+    {
+        $this->get('home')->assertRedirect('login');
     }
 }
