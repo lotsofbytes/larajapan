2cd2e3725d18f5fb07f9cbc207f5d55e3461011a
Kenji Hino <kenji@lotsofbytes.com>
2018-02-05 08:29:08 -0800

    l54-test2

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,14 +11,17 @@
 |
 */
 
+use Faker\Generator as Faker;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('test1234'),
+        'name'           => $faker->name,
+        'remember_token' => str_random(100),
     ];
 });
+
--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -5,10 +5,16 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+//use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use Auth;
+use App\User;
+
 
 class LoginTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function user_can_view_login()
     {
@@ -22,5 +28,55 @@ class LoginTest extends TestCase
     {
         $this->get('home')
         	->assertRedirect('login');
+    }
+
+    /** @test */
+    public function valid_user_can_login()
+    {
+        $user = factory(User::class)->create([
+            'password'  => bcrypt('test1111')
+        ]);
+
+        // まだ、認証されていない
+        $this->assertFalse(Auth::check());
+
+        // ログイン
+        $response = $this->post('login', [
+            'email'    => $user->email,
+            'password' => 'test1111'
+        ]);
+
+        // 認証されている
+        $this->assertTrue(Auth::check());
+
+        // ログイン後にホームページにリダイレクトされるのを確認
+        $response->assertRedirect('home');
+    }
+
+    /** @test */
+    public function invalid_user_cannot_login()
+    {
+        $user = factory(User::class)->create([
+            'password'  => bcrypt('test1111')
+        ]);
+
+        // まだ、認証されていない
+        $this->assertFalse(Auth::check());
+
+        // 異なるパスワードでログイン
+        $response = $this->post('login', [
+            'email'    => $user->email,
+            'password' => 'test2222'
+        ]);
+
+        // 認証失敗で、認証されていない
+        $this->assertFalse(Auth::check());
+
+        // セッションにエラーを含むことを確認
+        $response->assertSessionHasErrors(['email']);
+
+        // エラメッセージを確認
+        $this->assertEquals('メールアドレスあるいはパスワードが一致しません',
+            session('errors')->first('email'));
     }
 }
