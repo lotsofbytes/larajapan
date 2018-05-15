1a9a2b84df6f63aeea2d575dbd8a7967f5424295
Kenji Hino <kenji@lotsofbytes.com>
2018-05-14 18:20:15 -0700

    still working

--- tests/Unit/ValidationTest.php
+++ tests/Unit/ValidationTest.php
@@ -11,23 +11,28 @@ class ValidationTest extends TestCase
      * @test
 	 * @dataProvider provider_validate
      */
-    public function required_page($input, $expected)
+    public function required_age($input, $expected)
     {
         Validator::extendImplicit('required_age', function ($attribute, $value, $parameters) {
-           return ((strlen($value) > 0) || ($parameters[0] > 0));
+           return ((strlen($value) > 0) || ((int)$parameters[0] > 0));
         });
 
-        $v = Validator::make($input, ['date_birth' => 'required_page:age_group']);
+        $age_group = $input['age_group'];
 
-        $actual = !$v->fails();
+        $v = Validator::make($input, [
+            'date_birth' => 'required_age:" . $age_group . "|date'
+        ]);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $v->passes());
     }
 
     public function provider_validate()
     {
         return [
-            [['date_birth' => '2018-01-01', 'age_group' => null], true]
+            [['date_birth' => '2018-01-01', 'age_group' => '0'], true],
+            [['date_birth' => '2018-01-45', 'age_group' => '0'], false],
+            [['date_birth' => '', 'age_group' => '5'], true],
+            [['date_birth' => '', 'age_group' => '0'], false]
         ];
     }
 }
